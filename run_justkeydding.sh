b8e1cf9e98eb65cef1b89c2dd385ca4a95cfa71a
Nestor Napoles Lopez <napulen@gmail.com>
2020-05-07 23:41:23 -0400

    Adding code for evaluation and justkeydding predictions

--- run_justkeydding.sh
+++ run_justkeydding.sh
@@ -4,6 +4,6 @@ echo "file	output" > run_justkeydding.tsv
 for f in $(ls all_files | grep ".krn")
 do
 	echo $f
-	output=$(python3 -m justkeydding --json all_files/$f)
+	output=$(python3 -m justkeydding --majorEmission simple_harmonic_minor --minorEmission simple_harmonic_minor --transition ktg_exponential15 --json all_files/$f)
 	echo "$f	$output" >> run_justkeydding.tsv
 done

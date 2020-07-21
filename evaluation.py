f05083e9412221e59d48598a5ef959fbbb7ae26f
Nestor Napoles Lopez <napulen@gmail.com>
2020-07-21 15:11:32 -0400

    Adding raw evaluation

--- evaluation.py
+++ evaluation.py
@@ -6,10 +6,10 @@ import pandas as pd
 
 # Evaluable datasets
 datasets = [
-    'aldwell', 
+    'aldwell',
     'kostka-payne',
     'reger',
-    'rimsky-korsakov', 
+    'rimsky-korsakov',
     'tchaikovsky',
     'all_files'
 ]
@@ -25,11 +25,21 @@ models = [
 
 # Tasks to evaluate
 task_dict = {
-    'modulation': 'local_key_label', 
+    'modulation': 'local_key_label',
     'tonicization': 'tonicized_key_label'
 }
 
-def score_slice(evaldfrow):    
+def score_slice_mirex(evaldfrow):
+    gt, pred, _ = evaldfrow
+    score = utils.score_key_prediction_mirex(gt, pred)
+    return score
+
+def score_slice_mirex_with_duration(evaldfrow):
+    gt, pred, dur = evaldfrow
+    score = utils.score_key_prediction_mirex(gt, pred) * dur
+    return score
+
+def score_slice(evaldfrow):
     gt, pred, _ = evaldfrow
     score = utils.score_key_prediction(gt, pred)
     return score
@@ -39,10 +49,12 @@ def score_slice_with_duration(evaldfrow):
     score = utils.score_key_prediction(gt, pred) * dur
     return score
 
-# Evaluation metric
+# Evaluation metrics
 evaluation_metrics = {
+    'duration_based_mirex': score_slice_mirex_with_duration,
+    'slice_based_mirex': score_slice_mirex,
     'duration_based': score_slice_with_duration,
-    'slice_based': score_slice
+    'slice_based': score_slice,
 }
 
 def get_model(name, dataset_name, dfm, dft):
@@ -51,7 +63,7 @@ def get_model(name, dataset_name, dfm, dft):
     elif name == 'globalkey_guess':
         return utils.generate_globalkey_predictions(dataset_name)
     elif name == 'justkeydding':
-        return utils.justkeydding_to_dfdictionary(f'{dataset_name}_justkeydding.tsv')
+        return utils.justkeydding_to_dfdictionary(f'model_1_outputs/{dataset_name}_justkeydding.tsv')
     elif name == 'perfect_modulation':
         # _, dfm, _ = utils.load_dataset(dataset)
         return dfm
@@ -85,43 +97,45 @@ def pass_sanity_check(df, model):
 #   aldwell
 # 	...
 def evaluate_all():
-    scorelist = []    
+    scorelist = []
     for dataset_name in datasets:
         # Load the dataset only once (because it is slow)
-        df, dfm, dft = utils.load_dataset(dataset_name)        
-        for task_name in task_dict.keys():            
+        df, dfm, dft = utils.load_dataset(dataset_name)
+        for task_name in task_dict.keys():
             for model_name in models:
-                for evaluation_name in evaluation_metrics.keys():                                
+                for evaluation_name in evaluation_metrics.keys():
                     task = task_dict[task_name]
                     model = get_model(model_name, dataset_name, dfm, dft)
                     evaluation = evaluation_metrics[evaluation_name]
                     if not pass_sanity_check(df, model):
                         print(f"Can't evaluate model ${model_name}. Indexes are not correct.")
-                        quit()                    
-                    
+                        quit()
+
                     for f in list(df.keys()):
+                        print(f, evaluation_name, model_name, task_name, dataset_name)
                         # Create the evaluation dataframe
                         dfeval = pd.DataFrame(index=df[f].index)
                         dfeval['gt'] = df[f][task]
                         dfeval['pred'] = model[f].local_key_label
-                        dfeval['slice_duration'] = df[f].slice_duration    
+                        dfeval['slice_duration'] = df[f].slice_duration
                         dfeval['score'] = dfeval.apply(evaluation, axis=1, raw=True)
-                        if evaluation_name == 'slice_based':
+                        if 'slice_based' in evaluation_name:
                             score = dfeval['score'].sum() / dfeval.index.size
-                        elif evaluation_name == 'duration_based':
+                        elif 'duration_based' in evaluation_name:
                             score = dfeval['score'].sum() / dfeval.slice_duration.sum()
                         scorelist.append({
                             'dataset': dataset_name,
                             'file': f,
                             'task': task_name,
                             'model': model_name,
-                            'evaluation': evaluation_name,                            
-                            'mirex_score': score 
-                        })                        
+                            'evaluation': evaluation_name,
+                            'score': score
+                        })
                     # scoredf = pd.DataFrame(scoredict.values())
                     # print(scoredf.mean(), scoredf.std(), scoredf.min(), scoredf.max())
     return scorelist
 
-
+if __name__ == '__main__':
+    print(evaluate_all())
 
 
